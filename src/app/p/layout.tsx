2b11cc1637096eee67118dbd394964d52cd586e3
Asyraaf Amran <masyraaf@bankislam.com.my>
2025-02-05 04:35:08 +0800

    FEAT: additional pages

--- src/app/p/layout.tsx
+++ src/app/p/layout.tsx
@@ -17,21 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased relative overflow-clip`}
-      >
-        <div className="fixed inset-0 -z-10">
-          <Image src={backgroundImage} className="w-full h-full object-cover" alt="background Image" />
-          {/* Optional overlay for better readability */}
-          <div className="absolute inset-0 bg-black/05" />
-        </div>
+    <>
+      <div className="fixed inset-0 -z-10">
+        <Image src={backgroundImage} className="w-full h-full object-cover" alt="background Image" />
+        {/* Optional overlay for better readability */}
+        <div className="absolute inset-0 bg-black/05" />
+      </div>
 
-        <main className="h-screen overflow-scroll">{children}</main>
-        <div>
-          <Navbar />
-        </div>
-      </body>
-    </html>
+      <main className="h-screen overflow-scroll">{children}</main>
+      <div>
+        <Navbar />
+      </div>
+    </>
   );
 }
